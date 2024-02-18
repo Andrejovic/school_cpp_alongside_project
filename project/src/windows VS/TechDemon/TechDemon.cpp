0cc7771d3f15633140486b663f67ed2876f63ea0
Pribiš Andrej <ajdroidprihlaseny@gmail.com>
2024-02-18 12:31:25 +0000

    camera test

--- project/src/windows VS/TechDemon/TechDemon.cpp
+++ project/src/windows VS/TechDemon/TechDemon.cpp
@@ -1,4 +1,4 @@
-// TechDemon.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// TechnologicDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <opencv2/core.hpp>
@@ -6,11 +6,27 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 using namespace cv;
+using namespace std;
 
 int main()
 {
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cout << "cannot open camera";
+        return -1;
+    }
+    bool scanning = true;
+    while (scanning) {
+        cv::Mat frame;
+        cv::namedWindow("camera", WINDOW_AUTOSIZE);
+        cap.read(frame);
+        cv::imshow("camera", frame);
+        cv::waitKey(100);
+        scanning = getWindowProperty("camera", WND_PROP_VISIBLE) > 0;
+    }
+
     cv::Mat img = cv::imread("C:/Users/ajdro/Downloads/ilovecpp.png");
-    namedWindow("Loving cpp", WINDOW_AUTOSIZE);
+    cv::namedWindow("Loving cpp", WINDOW_AUTOSIZE);
     cv::imshow("Loving cpp", img);
     cv::moveWindow("Loving cpp", 0, 45);
     cv::waitKey(0);
