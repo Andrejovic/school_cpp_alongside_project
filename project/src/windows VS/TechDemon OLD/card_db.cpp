9f17dd1ec0286623e1fb7c76108b8d611a7a834d
Pribiš Andrej <ajdroidprihlaseny@gmail.com>
2024-04-27 17:08:46 +0000

    deleted old not-renamed files

--- project/src/windows VS/TechDemon OLD/card_db.cpp
+++
@@ -1,9 +0,0 @@
-#include "card_db.h"
-
-using namespace std;
-
-void card_db::add_to_collection(vector<string> scanned_cards) {
-	for (string card : scanned_cards) {
-		//api call to a website with prices
-	}
-}
